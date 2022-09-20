2ae4b382a31a9306a4d21c9293386e077f1a5040
Michael Chop <007micle007@gmail.com>
2022-09-20 13:44:06 +0300

    Uploading the project basics to github

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import style from './Header.module.css'
+
+const Header = () => {
+    return <header className={style.header}>
+        <img src="https://wallpapersmug.com/download/1280x1024/cdb250/stars-galaxy-nebula-interstellar-milky-way.jpg" />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import style from './Navbar.module.css'
+
+const Navbar = () => {
+    return <nav className={style.nav}>
+        <div className={`${style.item} ${style.active}`}>
+            <a>Profile</a>
+        </div>
+        <div className={style.item}>
+            <a>Messages</a>
+        </div>
+        <div className={style.item}>
+            <a>News</a>
+        </div>
+        <div className={style.item}>
+            <a>Music</a>
+        </div>
+        <div className={style.item}>
+            <a>Settings</a>
+        </div>
+    </nav>
+}
+
+export default Navbar;
