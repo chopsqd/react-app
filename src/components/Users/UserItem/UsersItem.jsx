48f2950f38503b81ad47fdeeb2a6b258b870786c
Michael Chop <007micle007@gmail.com>
2022-09-25 20:59:47 +0300

    Added Users API functionality using Axios

--- src/components/Users/UserItem/UsersItem.jsx
+++ src/components/Users/UserItem/UsersItem.jsx
@@ -1,10 +1,11 @@
 import style from './UsersItem.module.css'
+import userPhoto from '../../../assets/images/userPhoto.png'
 
 const UsersItem = (props) => {
     return <div className={style.usersItem}>
         <div>
             <div>
-                <img src={props.user.photoUrl}/>
+                <img src={props.user.photos.small ?? userPhoto}/>
                 {props.user.followed
                     ? <button onClick={() => {props.unfollow(props.user.id)}}>Unfollow</button>
                     : <button onClick={() => {props.follow(props.user.id)}}>Follow</button>}
@@ -12,10 +13,10 @@ const UsersItem = (props) => {
         </div>
 
         <div className={style.userInfo}>
-            <div>{props.user.fullName}</div>
+            <div>{props.user.name}</div>
             <div>{props.user.status}</div>
-            <div>{props.user.location.country}</div>
-            <div>{props.user.location.city}</div>
+            <div>{"props.user.location.country"}</div>
+            <div>{"props.user.location.city"}</div>
         </div>
     </div>
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,14 @@
 import style from './Users.module.css'
 import UsersItem from "./UserItem/UsersItem";
+import * as axios from "axios";
 
 const Users = (props) => {
     if(props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://i.pinimg.com/236x/59/c7/99/59c799e239940de4d573915d4abd47c4.jpg',
-                followed: false, fullName: 'Dmitry', status: 'Im boss', location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://i.pinimg.com/236x/59/c7/99/59c799e239940de4d573915d4abd47c4.jpg',
-                followed: true, fullName: 'Alex', status: 'ebal ya vas v rot', location: {city: 'Moskow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://i.pinimg.com/236x/59/c7/99/59c799e239940de4d573915d4abd47c4.jpg',
-                followed: false, fullName: 'Misha', status: 'Senior WebDev', location: {city: 'Donetsk', country: 'Russia'}
-            },
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
+
     }
 
     return props.users.map(user => <UsersItem key={user.id} user={user} follow={props.follow} unfollow={props.unfollow}/>)
