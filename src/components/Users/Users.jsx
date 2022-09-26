38d08b96059fe9f442ee734832d1786b0d137079
Michael Chop <007micle007@gmail.com>
2022-09-26 23:21:16 +0300

    Refactored code UsersContainer component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,44 @@
-import React from "react";
 import style from './Users.module.css'
-import UsersItem from "./UserItem/UsersItem";
-import * as axios from "axios";
+import userPhoto from '../../assets/images/userPhoto.png'
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            })
-    }
+    //Карусель массива номеров страниц
+    let slicedPages = pages.slice((((props.currentPage - 5) < 0) ? 0 : props.currentPage - 5), (props.currentPage + 5))
+    return (
+        <div>
+            {slicedPages.map(page => {
+                return <button
+                    key={page}
+                    className={props.currentPage === page ? style.selectedPage : ""}
+                    onClick={(event) => { props.onPageChanged(page)}}>{page}
+                </button>
+            })}
 
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-        let pages = [];
-        for(let i =1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
+            {props.users.map(user =>
+                <div className={style.usersItem} key={user.id}>
+                    <div>
+                        <img src={user.photos.small ?? userPhoto}/>
+                        { user.followed
+                        ? <button onClick={() => { props.unfollow(user.id)}}>Unfollow</button>
+                        : <button onClick={() => { props.follow(user.id)}}>Follow</button>}
+                    </div>
 
-        //Карусель массива номеров страниц
-        let slicedPages = pages.slice((((this.props.currentPage - 5) < 0) ?  0  : this.props.currentPage - 5), (this.props.currentPage + 5))
-
-        return (
-            <div>
-                { slicedPages.map(page => {
-                    return <button key={page} className={ this.props.currentPage === page ? style.selectedPage : "" } onClick={(event) => { this.onPageChanged(page)}}>{page}</button>
-                }) }
-
-                { this.props.users.map(user => <UsersItem key={user.id} user={user} follow={this.props.follow} unfollow={this.props.unfollow}/>) }
-            </div>
-        )
-    }
+                    <div className={style.userInfo}>
+                        <div>{user.name}</div>
+                        <div>{user.status}</div>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
+                    </div>
+                </div>)
+            }
+        </div>
+    )
 }
 
-export default Users
+export default Users;
