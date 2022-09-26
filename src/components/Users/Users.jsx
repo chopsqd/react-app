455909528fe839df060addb3fe42b9fbfd5b623e
Michael Chop <007micle007@gmail.com>
2022-09-26 22:04:47 +0300

    Added pagination functionality in the Users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,40 @@ import * as axios from "axios";
 
 class Users extends React.Component {
     componentDidMount() {
-        alert('MOUNTED')
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
 
     render() {
-        return this.props.users.map(user => <UsersItem key={user.id} user={user} follow={this.props.follow} unfollow={this.props.unfollow}/>)
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+        let pages = [];
+        for(let i =1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
+        //Карусель массива номеров страниц
+        let slicedPages = pages.slice((((this.props.currentPage - 5) < 0) ?  0  : this.props.currentPage - 5), (this.props.currentPage + 5))
+
+        return (
+            <div>
+                { slicedPages.map(page => {
+                    return <button key={page} className={ this.props.currentPage === page ? style.selectedPage : "" } onClick={(event) => { this.onPageChanged(page)}}>{page}</button>
+                }) }
+
+                { this.props.users.map(user => <UsersItem key={user.id} user={user} follow={this.props.follow} unfollow={this.props.unfollow}/>) }
+            </div>
+        )
     }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
-const SET_USERS = 'SET-USERS'
+const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
-    users: [ ]
+    users: [ ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -33,7 +38,19 @@ const profileReducer = (state = initialState, action) => {
         case SET_USERS:
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                //Если исп. users: [...state.users, ...action.users] - то будет добавлять в конец
+                // users: action.users перезаписывает данные
+                users: action.users
+            }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalUsersCount: action.totalUsersCount
             }
         default: return state;
     }
@@ -42,5 +59,7 @@ const profileReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount })
 
 export default profileReducer
