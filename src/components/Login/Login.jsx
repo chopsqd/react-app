4629cea5c976271b73762356953fa9f03a290f63
Michael Chop <007micle007@gmail.com>
2022-09-30 22:41:25 +0300

    Installed 'redux-form' package

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,27 @@
 import style from './Login.module.css'
 
+const LoginForm = (props) => {
+    return <form>
+        <div>
+            <input placeholder={"Login"}/>
+        </div>
+        <div>
+            <input placeholder={"Password"}/>
+        </div>
+        <div>
+            <input type={"checkbox"}/>Remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>Login</h1>
+        <LoginForm />
+    </div>
 }
 
 export default Login
