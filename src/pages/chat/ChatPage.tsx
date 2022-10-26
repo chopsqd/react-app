b132e42e222b635c80b92f7d1f7ba28221a627ed
Michael Chop <007micle007@gmail.com>
2022-10-26 23:45:43 +0300

    Added autoscroll to the chat

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {ChatMessageType} from "../../api/chat-api";
 import {useDispatch, useSelector} from "react-redux";
 import {sendMessage, startMessagesListening, stopMessagesListening} from "../../redux/chat-reducer";
@@ -12,7 +12,6 @@ const ChatPage: React.FC = () => {
 }
 
 const Chat: React.FC = () => {
-
     const dispatch = useDispatch()
     const status = useSelector((state: AppStateType) => state.chat.status)
 
@@ -24,21 +23,35 @@ const Chat: React.FC = () => {
     }, [])
 
     return <div>
-        {status === 'error'
-            ? <div>Some Error occurred...</div>
-            : <>
-                <Messages/>
-                <AddChatMessage/>
-            </>
-        }
+        {status === 'error' && <div>Some error occurred...</div>}
+        <Messages/>
+        <AddChatMessage/>
     </div>
 }
 
 const Messages: React.FC = () => {
     const messages = useSelector((state: AppStateType) => state.chat.messages)
+    const messagesRef = useRef<HTMLDivElement>(null)
+    const [isAutoScroll, setIsAutoScroll] = useState(true)
 
-    return <div style={{height: '400px', overflowY: 'auto'}}>
+    const scrollHandler = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const element = event.currentTarget
+        if(Math.abs((element.scrollHeight - element.scrollTop ) - element.clientHeight) < 300) {
+            !isAutoScroll && setIsAutoScroll(true)
+        } else {
+            isAutoScroll && setIsAutoScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        if(isAutoScroll) {
+            messagesRef.current?.scrollIntoView({behavior: 'smooth'})
+        }
+    }, [messages])
+
+    return <div style={{height: '400px', overflowY: 'auto'}} onScroll={scrollHandler}>
         {messages.map((m, index) => <Message key={index} message={m}/>)}
+        <div ref={messagesRef}></div>
     </div>
 }
 
